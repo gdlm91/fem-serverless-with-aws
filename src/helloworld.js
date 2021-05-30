300a86da03194e766e0726c273b10590bfde35fe
Gerardo Leal <gleal@atlassian.com>
2021-05-30 17:17:29 +1000

    hello lambda!

--- src/helloworld.js
+++ src/helloworld.js
@@ -1,3 +1,9 @@
 module.exports.handler = (evt, ctx, done) => {
-  
-}
+  console.log(evt);
+
+  if (evt.message !== "hello") {
+    done(new Error("you didn't say hello"), "working!!");
+  } else {
+    done(null, "working!!");
+  }
+};
